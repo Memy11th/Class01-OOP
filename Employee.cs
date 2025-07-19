5c2b6a6767b3f3805d2ad1fe4c67b1c2a9d41e20
Memy11th <abdelrahmanabdue11@gmail.com>
2025-07-19 19:26:13 +0300

    Applying Encapsulation to certain examples

---
+++ Employee.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class01_OOP
+{
+    internal struct Employee
+    {
+        #region Attributes
+        private int Id;
+        private string Name;
+        private decimal Salary;
+        #endregion
+
+        #region Constructors
+        public Employee(int _id, string _name, decimal _salary)
+        {
+            Id = _id;
+            Name = _name;
+            Salary = _salary;
+        }
+        #endregion
+
+        #region Methods
+        // Applying Encapsulation by getter and setter in Name
+        public string GetName()
+        {
+            return Name;
+        }
+
+        public void SetName(string _name)
+        {
+            Name = _name;
+        }
+
+        // Applying Encapsulation by Property 
+
+        public decimal Salaryy {
+            get { return Salary; }
+            set { Salary = value; }
+        }
+
+
+        public override string ToString()
+        {
+            return $"My name is {Name} and my code is {Id} meanwhile salary is {Salary}";
+        }
+        #endregion
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,6 +8,16 @@
             Point Node = new Point(1,3);
             Console.WriteLine(Node);
             #endregion
+
+            #region Encapsulation
+            Employee Emp01 = new Employee(1, "Memy", 25000);
+            Console.WriteLine(Emp01);
+            Emp01.SetName("Memy jr");
+            Console.WriteLine(Emp01);
+            Console.WriteLine(Emp01.Salaryy);
+            Emp01.Salaryy = 25;
+            Console.WriteLine(Emp01.Salaryy);
+            #endregion
         }
     }
 }
