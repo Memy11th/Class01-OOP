2f00de6470ed294178b70bd7471931703a0ab0fd
Memy11th <abdelrahmanabdue11@gmail.com>
2025-07-19 20:28:45 +0300

    PhoneBook task with indexer to be created

---
+++ PhoneBook.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class01_OOP
+{
+    internal struct PhoneBook
+    {
+        #region Attributes
+        string[] Names;
+        int[] PhoneNumero;
+        int Size;
+        #endregion
+
+        #region Constructor 
+            public PhoneBook(int size)
+        {
+            Size = size;
+            Names = new string[Size];
+            PhoneNumero = new int[Size];
+        }
+        #endregion
+
+        #region Methods
+            public void AddNewPerson(int Index,string Name,int Num)
+        {
+            Names[Index] = Name;
+            PhoneNumero[Index] = Num;
+        }
+        #endregion
+    }
+}
