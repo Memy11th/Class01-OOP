f9ac2e44b5b73c326c4dd24119323b7986381270
Memy11th <abdelrahmanabdue11@gmail.com>
2025-07-19 18:38:51 +0300

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class01_OOP
+{
+    internal struct Point
+    {
+        #region Attributes
+        public int X;
+        public int Y;
+        #endregion
+
+        #region Contstructors 
+            public Point()
+        {
+            X = 1;
+            Y = default;
+        }
+
+            public Point (int _x,int _y)
+        {
+            X = _x;
+            Y = _y;
+        }
+
+        public override string ToString()
+        {
+            return $"({X},{Y})";
+        }
+        #endregion
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Class01_OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Struct - OOP Class01
+            Point Node = new Point(1,3);
+            Console.WriteLine(Node);
+            #endregion
+        }
+    }
+}
