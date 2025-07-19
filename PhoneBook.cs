f824159b33e78610e1174156171c7f52c7b7634c
Memy11th <abdelrahmanabdue11@gmail.com>
2025-07-19 22:00:11 +0300

    Adding Getter methods

--- PhoneBook.cs
+++ PhoneBook.cs
@@ -26,9 +26,31 @@ namespace Class01_OOP
         #region Methods
             public void AddNewPerson(int Index,string Name,int Num)
         {
-            Names[Index] = Name;
-            PhoneNumero[Index] = Num;
+            if (Name is not null && PhoneNumero is not null)
+            {
+                if (Index <= 0 && Index < Names.Length)
+                {
+                    Names[Index] = Name;
+                    PhoneNumero[Index] = Num;
+                }
+            }
+            
         }
+
+            // Getter Fn
+            public int GetPhoneNum(string Name)
+        {
+            if(Name is not null && PhoneNumero is not null)
+                for(int i = 0; i < Names.Length; i++)
+                    if (Names[i] == Name)       
+                        return PhoneNumero[i];
+            return -1;
+        }
+        #endregion
+
+        #region Properties
+            
+
         #endregion
     }
 }
--- Program.cs
+++ Program.cs
@@ -5,8 +5,8 @@
         static void Main(string[] args)
         {
             #region Struct - OOP Class01
-            Point Node = new Point(1,3);
-            Console.WriteLine(Node);
+            //Point Node = new Point(1,3);
+            //Console.WriteLine(Node);
             #endregion
 
             #region Encapsulation
@@ -14,16 +14,23 @@
             // Easy to rename or edit source code in class 
             // Validation on data
             // Get or set or both 
-            Employee Emp01 = new Employee(1, "Memy", 25000);
-            Console.WriteLine(Emp01);
-            Emp01.SetName("Memy jr");
-            Console.WriteLine(Emp01);
-            Console.WriteLine(Emp01.Salaryy);
-            Emp01.Salaryy = 25;
-            Console.WriteLine(Emp01.Salaryy);
+            //Employee Emp01 = new Employee(1, "Memy", 25000);
+            //Console.WriteLine(Emp01);
+            //Emp01.SetName("Memy jr");
+            //Console.WriteLine(Emp01);
+            //Console.WriteLine(Emp01.Salaryy);
+            //Emp01.Salaryy = 25;
+            //Console.WriteLine(Emp01.Salaryy);
             #endregion
 
-
+            #region PhoneBook
+            PhoneBook Note = new PhoneBook(3);
+            Note.AddNewPerson(0, "Marwa", 111);
+            Note.AddNewPerson(1, "Memy jr", 222);
+            Note.AddNewPerson(2, "Body", 333);
+            
+            
+            #endregion
 
 
         }
